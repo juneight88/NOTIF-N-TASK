e9980d37655d0171dd5e642a446a388aeaba2ac0
juneight88 <159888068+juneight88@users.noreply.github.com>
2024-12-02 02:24:18 -0800

    Push
    First PUSh

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+
+const BACKGROUND_FETCH_TASK = 'background-fetch-task';
+
+export default function App() {
+    useEffect(() => {
+          const req
+    })
+}
